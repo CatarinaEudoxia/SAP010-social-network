63877a66775e3a3363985837cc7aacc12c373381
Cibellimonte <cibellimonte@gmail.com>
2023-07-20 16:48:05 -0300

    instalação do firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
 // Este es el punto de entrada de tu aplicacion
 
 import { myFunction } from './lib/index.js';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCQWC6WeCZe1egaAyDnI7EhcuUkYWmBclA",
+  authDomain: "wlamel-book-s.firebaseapp.com",
+  projectId: "wlamel-book-s",
+  storageBucket: "wlamel-book-s.appspot.com",
+  messagingSenderId: "608921320613",
+  appId: "1:608921320613:web:bfada4de699f662bcca14c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 
 myFunction();
