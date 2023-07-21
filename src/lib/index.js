0c33dba802bcfd4007aa03b7a9b6f114dba18375
CatarinaEudoxia <ceudoxia.ferreira@gmail.com>
2023-07-21 11:18:51 -0300

    Inicio do spa, construção das pastas

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,14 @@
+import home from "./home/index.js";
+import login from "./login/index.js";
+import cadastro from "./cadastro/index.js";
 // aqui exportaras las funciones que necesites
 
 export const myFunction = () => {
   // aqui tu codigo
   console.log('Hola mundo!');
 };
+
+
+window.addEventListener("load,")
+
+window.addEventListener("hashchange")
